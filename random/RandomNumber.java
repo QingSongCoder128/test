a049b18c0693895a7158bf711d3e87835cb30532
QingSongCoder128 <caoqingsong2025@outlook.com>
2026-03-12 20:11:05 +0800

    Update RandomNumber.java

--- random/RandomNumber.java
+++ random/RandomNumber.java
@@ -1,6 +1,6 @@
 public class RandomNumber {
     public static void main(String[] args) {
-    /*随机产生一个随机数，让它实现丢筛子的效果，每次的数字是1-6 Math.random()是随机产生【0 1）随机浮点数,
+    /*随机产生一个随机数，让它实现丢色子的效果，每次的数字是1-6 Math.random()是随机产生【0 1）随机浮点数,
         乘以6得到的区间是【0 6），能取到0取不到6显然和生活中的骰子不符合，所以+1变为【1 7）*/
         int random = (int) ((Math.random() * 6) + 1);
         System.out.println(random);
