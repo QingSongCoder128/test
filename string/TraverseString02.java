26e96de9f6cbfaf2bd4e59c8b57d0102cba47b8e
QingSongCoder128 <caoqingsong2025@outlook.com>
2026-03-22 13:02:02 +0800

    Create TraverseString02.java
    遍历字符串方式二

---
+++ string/TraverseString02.java
@@ -0,0 +1,8 @@
+public class TraverseString02 {
+    public static void main(String[] args) {
+        String s = "abcde";
+        for (int i = 0; i < s.length(); i++) {
+            System.out.print(s.charAt(i)+"\t");
+        }
+    }
+}
